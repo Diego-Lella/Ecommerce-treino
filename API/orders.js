84b55921de9b86a7dcb36afe42f522ec3c789c9d
Diego-Lella <diegocardoso234234@gmail.com>
2026-08-18 20:50:18 -0300

    Create orders.js

---
+++ API/orders.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+    return res.status(200).json({
+        funcionando: true,
+        metodo: req.method,
+        mensagem: "API funcionando corretamente"
+    });
+}
